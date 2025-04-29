3b738a18e76e76e6c82a8caf5f81879f586e8bc6
Joel Body <joel.body@proton.me>
2025-04-29 16:51:40 +0100

    moved some basic api functions to api/query.kt , this is the basic api query and string to json conversion.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,7 @@
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
-import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse
 
+import api.queryAPI
+import api.stringToJson
 
 @Serializable
 data class Password(val id: UInt, val website: String, val username: String, val password: String)
@@ -32,33 +28,7 @@ fun listAllPasswords(): List<Password> {
     return(tablePasswords)
 }
 
-// this will list convert a json string into a table of passwords
-fun stringToJson(json: String): List<Password> {
-    //deserializing the json value
-    val listPasswords = Json.decodeFromString<List<Password>>(json)
-    return(listPasswords)
 
-}
-// this queries the api with the required uri, with error handling
-fun queryAPI(url: String): String {
-    val client = HttpClient.newBuilder().build()
-    try {
-        val request = HttpRequest.newBuilder()
-            .uri(URI.create(url))
-            .build()
-        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
-        return(response.body())
-    } catch (e: Exception) {
-        System.err.println("" +
-                "-----------------------------------------------------\n" +
-                "                        :(\n" +
-                "Server has crashed ($e)\n" +
-                "Please reboot the password server\n" +
-                "-----------------------------------------------------")
-        return("")
-    }
-
-}
 
 const val socketWithPrefix: String = "http://192.168.1.120:7878"
 const val key: String = "seckey"
