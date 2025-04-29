6f190d360d84c28024d95ce1b3c3d482c865fa13
Joel Body <joel.body@proton.me>
2025-04-29 12:30:14 +0100

    taken returned json data from a string into List<Password>

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
@@ -12,14 +15,20 @@ fun main() {
 
 }
 
+@Serializable
+data class Password(val id: UInt, val website: String, val username: String, val password: String)
+
 fun listPasswords() {
+
     try {
         val request = HttpRequest.newBuilder()
             .uri(URI.create("http://192.168.1.120:7878/seckey/list_all/"))
             .build()
         val response = client.send(request, HttpResponse.BodyHandlers.ofString())
-        println(response.body())
-
+        val json = response.body() //this is the json return from the api query
+        //decoding the json and printing it
+        // val listPasswords = Json.decodeFromString<List<Password>>(json)
+        println(listPasswords)
     } catch (e: Exception) {
         System.err.println("Server has crashed ($e)")
     }
