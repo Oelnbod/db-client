eb41a8241bb9df19cd8ee1e17203487c8837ab31
Joel Body <joel.body@proton.me>
2025-05-10 17:46:41 +0100

    Printing listAll as a table, yet to migrate to a separate function or module.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,32 +5,44 @@ import api.listSomePasswords
 import java.util.Arrays
 
 fun main() {
+    //parsing it to independent lists by column
     val data = listAllPasswords()
     val idList: List<String> = (data.map { it.id.toString() })
     val websiteList: List<String> = data.map { it.website }
     val usernameList: List<String> = data.map { it.username }
     val passwordList: List<String> = data.map { it.password }
-    //println(idList + websiteList + usernameList + passwordList)
+
+    //merging lists together through .zip
     val fullList = idList.zip(websiteList.zip(usernameList.zip(passwordList)))
+    //converting to an array for convenience
     val fullArray: Array<Array<String>> = fullList.map {
         arrayOf(it.first, it.second.first, it.second.second.first, it.second.second.second)
     }.toTypedArray()
-    println("[id, website, username, password]")
 
-    for (items in fullArray) {
-        val stringItem = items.contentDeepToString() //converts to a string
-        val item = stringItem.replace(",", "  |  ") //formatting into table
-        //this removes the brackets
-        val endPoint = item.length-1
-        println(item.substring(1, endPoint))
+    //identifying length so can be referenced by index
+    val lengthAll: List<Int> =
+        listOf(largest(idList), largest(websiteList), largest(usernameList), largest(passwordList))
+    for (row in fullArray) {
+        //println()
+
+        for (item in row) {
+            //getting a limit for padding from list above
+            val paddingLimit: Int = lengthAll[row.indexOf(item)]
+            //padding using limit above
+            val stringItem = padding(item, paddingLimit)
+
+            //inserting a pipe beforehand
+            print("| $stringItem  ")
 
 
+        }
+        //adding a pipe to the end of each row and a blank line
+        print("|")
+        println()
     }
 
 
-
-
-
+// other commands available
 //    println(addPasswords("p.p", "uid", "pwd"))
 //    println(listAllPasswords())
 //    println(listSomePasswords("example.com"))
@@ -38,3 +50,29 @@ fun main() {
 //    println(listAllPasswords())
 }
 
+fun largest(list: List<String>): Int {
+    //this works by adding to a list and then sorting, reversing and taking the first value
+    val lengthsList = mutableListOf<Int>()
+    for (items in list) {
+        lengthsList.add(items.length)
+
+    }
+    return (lengthsList.sorted().reversed()[0])
+
+}
+
+fun padding(input: String, target: Int): String {
+    //This is for a consistent length of column
+    var input = input
+    var suitableLength = false
+    //println("TARGET:  $target")
+    while (!suitableLength) {
+        if (input.length == target) {
+            suitableLength = true
+        } else {
+            input = "$input "
+            //println(input)
+        }
+    }
+    return (input)
+}
