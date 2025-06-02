f4b62b55c779f97bc19fa50743fac23fea45e873
Joel Body <joel.body@proton.me>
2025-06-02 12:40:17 +0100

    Added basic decision functionality.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,50 @@
 import api.Password
+import api.addPasswords
+import api.deletePasswords
 import api.listAllPasswords
+import api.listSomePasswords
 
 fun main() {
 
-    val data = listAllPasswords()
-    println("Passwords: \n" + displayAsTable(data))
+
+    println("All passwords listed below: \n" + displayAsTable(listAllPasswords()))
+    println("please enter a number that corresponds to your preferred choice: \n 1: display all passwords again \n 2: search by website \n 3: Add a password \n 4: Delete a password")
+    val input = readLine()
+    println(input)
+    when (input) {
+        "1" -> println(displayAsTable(listAllPasswords()))
+        "2" -> {
+            println("Enter website to search for:")
+            val website = readln()
+            println(displayAsTable(listSomePasswords(website)))
+        }
+
+        "3" -> {
+            println("Enter your website")
+            val website = readln()
+            println("Enter your username")
+            val username = readln()
+            println("Enter your password")
+            val password = readln()
+            println(addPasswords(website, username, password))
+        }
+
+        "4" -> {
+            println("Enter the website of the password that you want to delete")
+            val website = readln()
+            println("Are you sure? (Y/n)")
+            val approved = readln()
+            if (approved == "Y") {
+                println(deletePasswords(website))
+            } else {
+                println("Canceled")
+            }
+        }
+
+        else -> println("invalid")
+    }
 
 
-
-// other commands available
-//   println(addPasswords("p.p", "uid", "pwd"))
-//   println(listAllPasswords())
-//   println(listSomePasswords("example.com"))
-//   println(deletePasswords("p.p"))
-//    println(listAllPasswords())
 }
 
 fun largest(list: List<String>): Int {
